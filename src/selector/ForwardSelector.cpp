3f53ef76a96d4befa6302a8a47cfd3fbcba59997
Aditya Vamsikrishna Mandalika <adityavk711@gmail.com>
2019-02-16 13:00:35 -0800

    fix the forward selector

--- src/selector/ForwardSelector.cpp
+++ src/selector/ForwardSelector.cpp
@@ -1,5 +1,7 @@
 #include "gls/selector/ForwardSelector.hpp"
 
+#include <algorithm> // std::reverse
+
 namespace gls {
 namespace selector {
 
@@ -20,20 +22,23 @@ Path ForwardSelector::selectEdgesToEvaluate(gls::datastructures::Path path)
 {
   // If the path is to the target, evaluate the entire path.
   if (path[0] == mTargetVertex)
+  {
+    std::reverse(path.begin(), path.end());
     return path;
+  }
 
   // Else return the first unevaluated edge closest to source.
   Path edgesToEvaluate;
-  for (std::size_t i = 0; i < path.size() - 1; ++i)
+  for (std::size_t i = path.size()-1; i > 0; --i)
   {
     Edge uv;
     bool edgeExists;
-    boost::tie(uv, edgeExists) = edge(path[i], path[i + 1], mGraph);
+    boost::tie(uv, edgeExists) = edge(path[i], path[i - 1], mGraph);
 
     if (mGraph[uv].getEvaluationStatus() == EvaluationStatus::NotEvaluated)
     {
       edgesToEvaluate.emplace_back(path[i]);
-      edgesToEvaluate.emplace_back(path[i + 1]);
+      edgesToEvaluate.emplace_back(path[i - 1]);
       break;
     }
   }
