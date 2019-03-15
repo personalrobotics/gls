0f5b0add92cd0fdc0a23fc04a888d931a5249f49
Aditya Vamsikrishna Mandalika <adityavk711@gmail.com>
2019-03-15 14:44:31 -0700

    add alternate selector

---
+++ src/selector/AlternateSelector.cpp
@@ -0,0 +1,44 @@
+#include "gls/selector/AlternateSelector.hpp"
+#include "gls/selector/BackwardSelector.hpp"
+#include "gls/selector/ForwardSelector.hpp"
+
+namespace gls {
+namespace selector {
+
+using gls::datastructures::Path;
+using gls::datastructures::Vertex;
+
+//==============================================================================
+AlternateSelector::AlternateSelector()
+{
+  // Setup forward selector.
+  mForwardSelector = std::make_shared<gls::selector::ForwardSelector>();
+  mForwardSelector->setup(mGraph, mSourceVertex, mTargetVertex);
+
+  // Setup backward selector.
+  mBackwardSelector = std::make_shared<gls::selector::BackwardSelector>();
+  mBackwardSelector->setup(mGraph, mSourceVertex, mTargetVertex);
+}
+
+//==============================================================================
+Path AlternateSelector::selectEdgesToEvaluate(gls::datastructures::Path path)
+{
+  // Flip the boolean.
+  mUseForwardSelector = !mUseForwardSelector;
+
+  if (mUseForwardSelector)
+    return mForwardSelector->selectEdgesToEvaluate(path);
+  else
+    return mBackwardSelector->selectEdgesToEvaluate(path);
+}
+
+//==============================================================================
+Path AlternateSelector::rankEdgesByUtilityInEvaluation(
+    gls::datastructures::Path& path)
+{
+  // Do nothing.
+  return path;
+}
+
+} // namespace selector
+} // namespace gls
