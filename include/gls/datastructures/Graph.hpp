ef43df87cd14947125416d1e92b50722fd308a78
Aditya Vamsikrishna Mandalika <adityavk@cs.uw.edu>
2020-06-07 12:04:57 -0700

    add edge struct

--- include/gls/datastructures/Graph.hpp
+++ include/gls/datastructures/Graph.hpp
@@ -26,8 +26,33 @@ struct Node {
   /// Flag indicates if the node is in repair.
   bool repair;
 
+  /// Children in the search tree.
+  std::vector<std::size_t> children;
+
   /// Returns the total cost of the node.
   constexpr double totalCost() const { return costToCome + heuristic; };
+
+  /// Utility function to remove a child from \c children.
+  inline void removeChild(const std::size_t& id) {
+    for (auto i = 0; i < children.size(); ++i) {
+      if (children[i] == id) {
+        children[i] = children.back();
+        children.pop_back();
+      }
+    }
+  }
+};
+
+/// \brief Represents the edge properties in the graph.
+struct Edge {
+  /// Edge length.
+  double length;
+
+  /// Collision status of the edge.
+  bool collision;
+
+  /// Evaluation status of the edge.
+  bool evaluated;
 };
 
 }  // namespace gls
