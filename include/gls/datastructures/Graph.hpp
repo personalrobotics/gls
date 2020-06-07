1d9f3533be84deaab63f10664df9bedc818f4760
Aditya Vamsikrishna Mandalika <adityavk@cs.uw.edu>
2020-06-07 01:15:02 -0700

    add doscstrings for node

--- include/gls/datastructures/Graph.hpp
+++ include/gls/datastructures/Graph.hpp
@@ -6,14 +6,27 @@
 
 namespace gls {
 
+/// \brief Node corresponding to a vertex in the graph.
 struct Node {
+  /// Node ID.
   std::size_t id;
+
+  /// Cost-to-Come.
   double costToCome;
+
+  /// Heuristic.
   double heuristic;
+
+  /// Flag indicates if the node is in collision.
   bool collision;
+
+  /// Flag indicates if the node has been expanded before.
   bool expanded;
+
+  /// Flag indicates if the node is in repair.
   bool repair;
 
+  /// Returns the total cost of the node.
   constexpr double totalCost() const { return costToCome + heuristic; };
 };
 
