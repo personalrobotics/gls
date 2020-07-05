159f68987a16eb05102971d39537453d3c5c99a0
Aditya Vamsikrishna Mandalika <adityavk@cs.uw.edu>
2020-07-04 19:03:16 -0700

    add tests for priority queue

--- tests/datastructures/PriorityQueueTest.cpp
+++ tests/datastructures/PriorityQueueTest.cpp
@@ -6,202 +6,182 @@
 class PriorityQueueTest : public ::testing::Test {
  protected:
   /// Setup the test fixture.
-  void SetUp() override {  // Do nothing.
-  }
+  void SetUp() override{};
 };
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, CreateEmptyNamedQueue) {
-  auto searchQueue = gls::PriorityQueue<gls::FValueComparator>();
+  gls::Graph g;
+  auto searchQueue = gls::PriorityQueue(g);
   EXPECT_EQ(searchQueue.getName(), "PriorityQueue");
   EXPECT_TRUE(searchQueue.isEmpty());
 
-  auto namedSearchQueue = gls::PriorityQueue<gls::FValueComparator>("name");
+  auto namedSearchQueue = gls::PriorityQueue(g, "name");
   EXPECT_EQ(namedSearchQueue.getName(), "name");
   EXPECT_TRUE(namedSearchQueue.isEmpty());
 }
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, AddNode) {
-  auto searchQueue = gls::PriorityQueue<gls::FValueComparator>();
+  gls::Graph g;
+  auto searchQueue = gls::PriorityQueue(g);
+  auto s = boost::add_vertex(g);
   EXPECT_TRUE(searchQueue.isEmpty());
-  searchQueue.addNode(gls::Node());
+  searchQueue.addNode(s);
   EXPECT_FALSE(searchQueue.isEmpty());
 }
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, HasNode) {
-  auto searchQueue = gls::PriorityQueue<gls::FValueComparator>();
-  gls::Node n;
-  n.costToCome = 1;
-  searchQueue.addNode(n);
-  EXPECT_TRUE(searchQueue.hasNode(n));
+  gls::Graph g;
+  auto searchQueue = gls::PriorityQueue(g);
+  auto s = boost::add_vertex(g);
+  searchQueue.addNode(s);
+  EXPECT_TRUE(searchQueue.hasNode(s));
 }
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, PopNode) {
-  auto searchQueue = gls::PriorityQueue<gls::FValueComparator>();
+  gls::Graph g;
+  auto s = boost::add_vertex(g);
+  auto searchQueue = gls::PriorityQueue(g);
   EXPECT_TRUE(searchQueue.isEmpty());
-  searchQueue.addNode(gls::Node());
+  searchQueue.addNode(s);
   EXPECT_FALSE(searchQueue.isEmpty());
   searchQueue.popTopNode();
   EXPECT_TRUE(searchQueue.isEmpty());
 }
 
 // ==============================================================================
-TEST_F(PriorityQueueTest, CompareFValue) {
-  auto searchQueue = gls::PriorityQueue<gls::FValueComparator>();
+TEST_F(PriorityQueueTest, CompareID) {
+  gls::Graph g;
+  auto s = boost::add_vertex(g);
+  auto t = boost::add_vertex(g);
+  auto searchQueue = gls::PriorityQueue(g);
 
-  // Key: 4
-  gls::Node n1;
-  n1.costToCome = 1;
-  n1.heuristic = 3;
-
-  // Key: 3
-  gls::Node n2;
-  n2.costToCome = 2;
-  n2.heuristic = 1;
-
-  // Key: 2
-  gls::Node n3;
-  n3.costToCome = 2;
-  n3.heuristic = 0;
-
-  searchQueue.addNode(n1);
-  searchQueue.addNode(n2);
-  searchQueue.addNode(n3);
+  searchQueue.addNode(s);
+  searchQueue.addNode(t);
 
   EXPECT_FALSE(searchQueue.isEmpty());
 
   // Check the order in the queue.
-  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 2);
-  searchQueue.removeNode(n3);
-  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 3);
-  searchQueue.removeNode(n2);
-  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 4);
-  searchQueue.removeNode(n1);
+  EXPECT_EQ(searchQueue.getTopNode(), s);
+  searchQueue.removeNode(s);
+  EXPECT_EQ(searchQueue.getTopNode(), t);
+  searchQueue.removeNode(t);
 
   EXPECT_TRUE(searchQueue.isEmpty());
 }
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, CompareGValue) {
-  auto searchQueue = gls::PriorityQueue<gls::GValueComparator>();
+  gls::Graph g;
+  auto s = boost::add_vertex(g);
+  auto v = boost::add_vertex(g);
+  auto t = boost::add_vertex(g);
+  auto searchQueue = gls::GValueQueue(g);
 
   // Key: 1
-  gls::Node n1;
-  n1.costToCome = 1;
-  n1.heuristic = 3;
+  g[s].costToCome = 1;
+  g[s].heuristic = 3;
 
   // Key: 4
-  gls::Node n2;
-  n2.costToCome = 4;
-  n2.heuristic = 1;
+  g[v].costToCome = 4;
+  g[v].heuristic = 1;
 
   // Key: 2
-  gls::Node n3;
-  n3.costToCome = 2;
-  n3.heuristic = 0;
+  g[t].costToCome = 2;
+  g[t].heuristic = 0;
 
-  searchQueue.addNode(n1);
-  searchQueue.addNode(n2);
-  searchQueue.addNode(n3);
+  searchQueue.addNode(s);
+  searchQueue.addNode(v);
+  searchQueue.addNode(t);
 
   EXPECT_FALSE(searchQueue.isEmpty());
 
   // Check the order in the queue.
   EXPECT_EQ(searchQueue.getTopNodeCostToCome(), 1);
-  searchQueue.removeNode(n1);
+  EXPECT_EQ(searchQueue.getTopNode(), s);
+  searchQueue.removeNode(s);
+
   EXPECT_EQ(searchQueue.getTopNodeCostToCome(), 2);
-  searchQueue.removeNode(n3);
+  EXPECT_EQ(searchQueue.getTopNode(), t);
+  searchQueue.removeNode(t);
+
   EXPECT_EQ(searchQueue.getTopNodeCostToCome(), 4);
-  searchQueue.removeNode(n2);
+  EXPECT_EQ(searchQueue.getTopNode(), v);
+  searchQueue.removeNode(v);
+
+  EXPECT_TRUE(searchQueue.isEmpty());
+}
+
+// ==============================================================================
+TEST_F(PriorityQueueTest, CompareFValue) {
+  gls::Graph g;
+  auto s = boost::add_vertex(g);
+  auto v = boost::add_vertex(g);
+  auto t = boost::add_vertex(g);
+  auto searchQueue = gls::FValueQueue(g);
+
+  // Key: 4
+  g[s].costToCome = 1;
+  g[s].heuristic = 3;
+
+  // Key: 5
+  g[v].costToCome = 4;
+  g[v].heuristic = 1;
+
+  // Key: 2
+  g[t].costToCome = 2;
+  g[t].heuristic = 0;
+
+  searchQueue.addNode(s);
+  searchQueue.addNode(v);
+  searchQueue.addNode(t);
+
+  EXPECT_FALSE(searchQueue.isEmpty());
+
+  // Check the order in the queue.
+  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 2);
+  EXPECT_EQ(searchQueue.getTopNode(), t);
+  searchQueue.removeNode(t);
+
+  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 4);
+  EXPECT_EQ(searchQueue.getTopNode(), s);
+  searchQueue.removeNode(s);
+
+  EXPECT_EQ(searchQueue.getTopNodeTotalCost(), 5);
+  EXPECT_EQ(searchQueue.getTopNode(), v);
+  searchQueue.removeNode(v);
 
   EXPECT_TRUE(searchQueue.isEmpty());
 }
 
 // ==============================================================================
 TEST_F(PriorityQueueTest, TieBreaking) {
-  auto fqueue = gls::PriorityQueue<gls::FValueComparator>();
-  auto gqueue = gls::PriorityQueue<gls::GValueComparator>();
+  gls::Graph g;
+  auto s = boost::add_vertex(g);
+  auto t = boost::add_vertex(g);
 
-  // [g, f] = [1,2]
-  gls::Node n1;
-  n1.id = 1;
-  n1.costToCome = 1;
-  n1.heuristic = 2;
+  auto fqueue = gls::FValueQueue(g);
+  auto gqueue = gls::GValueQueue(g);
 
-  // [g, f] = [1,2]
-  gls::Node n2;
-  n2.id = 2;
-  n2.costToCome = 1;
-  n2.heuristic = 2;
+  g[s].costToCome = 1;
+  g[s].heuristic = 2;
 
-  gqueue.addNode(n1);
-  gqueue.addNode(n2);
+  g[t].costToCome = 1;
+  g[t].heuristic = 2;
 
-  fqueue.addNode(n1);
-  fqueue.addNode(n2);
+  gqueue.addNode(s);
+  gqueue.addNode(t);
+
+  fqueue.addNode(s);
+  fqueue.addNode(t);
 
   EXPECT_FALSE(fqueue.isEmpty());
   EXPECT_FALSE(gqueue.isEmpty());
 
   // Check the order in the queue.
-  gls::Node topNode = fqueue.popTopNode();
-  EXPECT_EQ(topNode.id, 1u);
-  gls::Node bottomNode = fqueue.popTopNode();
-  EXPECT_EQ(bottomNode.id, 2u);
-  EXPECT_TRUE(fqueue.isEmpty());
-
-  topNode = gqueue.popTopNode();
-  EXPECT_EQ(topNode.id, 1u);
-  bottomNode = gqueue.popTopNode();
-  EXPECT_EQ(bottomNode.id, 2u);
-  EXPECT_TRUE(gqueue.isEmpty());
-}
-
-// ==============================================================================
-TEST_F(PriorityQueueTest, UpdateKeyValue) {
-  auto searchQueue = gls::PriorityQueue<gls::GValueComparator>();
-
-  // Key: 1
-  gls::Node n1;
-  n1.costToCome = 1;
-
-  // Key: 4
-  gls::Node n2;
-  n2.costToCome = 4;
-
-  // Key: 2
-  gls::Node n3;
-  n3.costToCome = 2;
-
-  searchQueue.addNode(n1);
-  searchQueue.addNode(n2);
-  searchQueue.addNode(n3);
-  EXPECT_EQ(searchQueue.getSize(), 3u);
-
-  // Check the order in the queue.
-  gls::Node r1 = searchQueue.popTopNode();
-  EXPECT_EQ(r1.costToCome, 1);
-  gls::Node r2 = searchQueue.popTopNode();
-  EXPECT_EQ(r2.costToCome, 2);
-  gls::Node r3 = searchQueue.popTopNode();
-  EXPECT_EQ(r3.costToCome, 4);
-  EXPECT_TRUE(searchQueue.isEmpty());
-
-  // Update the key for n1
-  searchQueue.addNode(n1);
-  searchQueue.addNode(n2);
-  searchQueue.addNode(n3);
-  EXPECT_EQ(searchQueue.getSize(), 3u);
-
-  searchQueue.updateNodeCostToCome(n1, 5);
-  r1 = searchQueue.popTopNode();
-  EXPECT_EQ(r1.costToCome, 2);
-  r2 = searchQueue.popTopNode();
-  EXPECT_EQ(r2.costToCome, 4);
-  r3 = searchQueue.popTopNode();
-  EXPECT_EQ(r3.costToCome, 5);
-  EXPECT_TRUE(searchQueue.isEmpty());
+  EXPECT_EQ(gqueue.getTopNode(), s);
+  EXPECT_EQ(fqueue.getTopNode(), s);
 }
